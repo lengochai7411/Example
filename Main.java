2d3854b78b032f80b568042ae544c44935f2a59b
Ngoc Hai <ngochai.07042001@gmail.com>
2021-03-21 19:42:34 +0700

    upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.qlks.main;
+
+import com.company.qlks.login.LoginJFrame;
+import com.company.qlks.view.MainJFrame;
+
+/**
+ *
+ * @author PHONG VU
+ */
+public class Main {
+    public static void main(String[] args) {
+        new LoginJFrame().setVisible(true);
+    }
+}
